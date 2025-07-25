5dd96d9bce9e8eff34ee2cf93751a72567c66a91
kal-kyokya <kalkyokya4@gmail.com>
2025-07-25 12:31:16 +0000

    Add comments to the ip_tracking middleware describing what crucial code lines are about.

--- alx_backend_security/ip_tracking/middleware.py
+++ alx_backend_security/ip_tracking/middleware.py
@@ -7,12 +7,15 @@ logger = logging.getLogger(__name__)
 class IPLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        # One-time configuration and initialization
 
     def __call__(self, request):
+        # Code to be executed for each request before the view is called
         self.process_request(request)
 
         response = self.get_response(request)
 
+        # Code to be executed for each request after the view is called
         return response
 
 def process_request(self, request):
@@ -20,8 +23,10 @@ def process_request(self, request):
     """
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
+        # If behind a proxy, client IP is usually the first in the list
         ip_address = x_forwarded_for.split(',')[0].strip()
     else:
+        # Fallback for direct connections
         ip_address = request.META.get('REMOTE_ADDR')
 
     path = request.path
